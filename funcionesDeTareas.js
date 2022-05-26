2afbe471b4f01627e0519357be448fa0c6f24a2b
pablosimkievich <pablosimkievich@gmail.com>
2022-05-26 12:13:29 -0300

    tercer commit actualizacion funcionesDeTareas

--- funcionesDeTareas.js
+++ funcionesDeTareas.js
@@ -7,20 +7,21 @@ let archivoTareas = {
         return JSON.parse(tareas);
     },
     escribirJSON: function () {
-    
+        let leerArchivo = this.leerArchivo ();
+        let archivoString = JSON.stringify (leerArchivo);
+        let escribirArchivo = fs.writeFileSync ('tareas.json', archivoString );
+        return escribirArchivo
     },
     guardarTarea: function (nuevoObjeto) {
-        let readfile = fs.readFileSync ('tareas.json', 'utf-8');
-        let readParseado = JSON.parse (readfile);
-        let spreadOperated = [...readParseado, nuevoObjeto];
+        let readArchivoParseado = this.leerArchivo(); 
+        let spreadOperated = [...readArchivoParseado, nuevoObjeto];
         let nuevoArrayJSON = JSON.stringify (spreadOperated);
         let tareaWrite = fs.writeFileSync ('tareas.json', nuevoArrayJSON);
         return tareaWrite;
     },
     leerPorEstado: function (estadoConsola) {
-        let readfile = fs.readFileSync ('tareas.json', 'utf-8');
-        let fileParseado = JSON.parse (readfile);
-        let resultadoFiltrado = fileParseado.filter (function (element) {
+        let readFileParseado = this.leerArchivo()
+        let resultadoFiltrado = readFileParseado.filter (function (element) {
         return  element.estado === estadoConsola    
         });
         return resultadoFiltrado;
